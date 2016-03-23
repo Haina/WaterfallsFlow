941949429dd33e98d5972e4e42a3058728852621
Hanna <Hanna@HannadeMacBook-Pro.local>
2016-03-23 21:46:47 +0800

    初始化项目

---
+++ WaterFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterFall
+//
+//  Created by Hanna on 16/3/23.
+//  Copyright © 2016年 Hanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaterFall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaterFall
+//
+//  Created by Hanna on 16/3/23.
+//  Copyright © 2016年 Hanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
